b1b57f8868a633f6d36b951efe260c00dd65c4ec
Lee Pei Yee <pei.lee-3@student.manchester.ac.uk>
2025-10-23 16:09:28 +0100

    Add comments

--- bash_script_pull.sh
+++ bash_script_pull.sh
@@ -6,3 +6,6 @@ set -euo pipefail
 
 curl -sSL "https://raw.githubusercontent.com/pei-yee03/FYP-Test/main/model_roles/tester" -o "testing_file"
 # Structure of URL should be "https://raw.githubusercontent.com/${user}/${repo}/${branch}/${filepath}"
+# "s" tells script to hide progress meter and most output. Silent mode
+# "S" tells script to print errors if something goes wrong. Show errors
+# "L" tells scripts to follow redirects, because some URLs don't serve the files directly. 
